112cfe4cde33570b104ee9cb264c74a819cf8796
dasariraju3344 <dasariraju3344@gmail.com>
2024-08-12 14:36:35 +0530

    Update post-list-store.jsx

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -57,15 +57,15 @@ const PostListProvider = ({ children }) => {
 const DEFAULT_POST_LIST = [
   {
     id: "1",
-    title: "Going to Mumbai",
-    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
-    reactions: 2,
+    title: "Going to Mumbai with Hari Kristna",
+    body: "Hi Friends, I am going to Mumbai for my vacations with my friend Hari. Hope to enjoy a lot. Peace out.",
+    reactions: 9,
     userId: "user-9",
     tags: ["vacation", "Mumbai", "Enjoying"],
   },
   {
     id: "2",
-    title: "Paas ho bhai",
+    title: "Hari completed Degree",
     body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
     reactions: 15,
     userId: "user-12",
